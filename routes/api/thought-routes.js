b1ad60096641cd779be880aa64f8fd92df4de999
Joshua Rangai <rangaij0116@hotmail.com>
2022-07-04 16:29:03 -0500

    add and remove reaction to routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,10 +2,16 @@ const router = require("express").Router();
 const {
   addThought,
   removeThought,
+  addReaction,
+  removeReaction,
 } = require("../../controllers/thought-controller");
 
 router.route("/:userId").post(addThought);
 
 router.route("/:userId/:thoughtId").delete(removeThought);
+
+router.route("/:userId/:reactionId").put(addReaction).delete(removeReaction);
+
+router.route("/:userId/:thoughtId/:reactionId").delete(removeReaction);
 
 module.exports = router;
