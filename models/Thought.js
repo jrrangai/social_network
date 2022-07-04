82136736c6cbc9f1710c1f07ef12c6ce4be1e214
Joshua Rangai <rangaij0116@hotmail.com>
2022-07-04 15:58:39 -0500

    added reaction model as subdocument

--- models/Thought.js
+++ models/Thought.js
@@ -1,50 +1,64 @@
 const { Schema, model, Types } = require("mongoose");
-const dateFormat = =require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
-const ReactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reationBody: {
+      type: String,
+      required: [true, "Must have valid text"],
+      maxlength: [280, "Cannot exceed 280 characters"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  reationBody: {
-    type: String,
-    required: [true, "Must have valid text"],
-    maxlength: [280, "Cannot exceed 280 characters"],
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal),
-  },
-});
-
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: [true, "Text is required"],
-    minlength: [1, "At least 1 character is needed"],
-    maxlength: [280, "Cannnot exceed 280 characters"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal),
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-  },
-  reactions: [],
-},
-{
-  toJson: {
-    virtuals: true,
-    getters: true,
+  {
+    toJSON: {
+      getters: true,
+    },
   }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: [true, "Text is required"],
+      minlength: [1, "At least 1 character is needed"],
+      maxlength: [280, "Cannnot exceed 280 characters"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJson: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 const Thought = model("Thought", ThoughtSchema);
