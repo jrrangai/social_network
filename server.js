b385343d83bab1fe1c2979468f493932985cbc2f
Joshua Rangai <rangaij0116@hotmail.com>
2022-07-04 13:25:56 -0500

    models

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = requrie('express');
-const mongoose = require('mongoose');
+const express = requrie("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social_network'. {
+// tells mongoose which databse we want to connect to. If URI exists then we'll use it, if not it will connect to /social_network
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social_network",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 
 // Log mongo queries being exectued
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
